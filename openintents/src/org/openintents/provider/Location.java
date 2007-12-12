7126913533e987fbb043cc7a2d2ef92f58acd4d3
friedger <friedger@gmail.com>
2007-12-12 22:16:07 +0000

    initial checkin based on NotePad
    git-svn-id: https://openintents.googlecode.com/svn/trunk@59 72b678ce-9140-0410-bee8-679b907dd61a

---
+++ openintents/src/org/openintents/provider/Location.java
@@ -0,0 +1,45 @@
+package org.openintents.provider;
+
+import android.net.ContentURI;
+import android.provider.BaseColumns;
+
+public abstract class Location {
+	
+	public static final class Locations implements BaseColumns {
+		/**
+         * The content:// style URL for this table
+         */
+        public static final ContentURI CONTENT_URI
+                = ContentURI.create("content://org.openintents.locations/locations");
+
+        /**
+         * The default sort order for this table
+         */
+        public static final String DEFAULT_SORT_ORDER = "modified DESC";
+        
+       
+        /**
+         * The latitude of the location
+         * <P>Type: TEXT</P>
+         */
+        public static final String LATITUDE = "latitude";
+
+        /**
+         * The longitude of the location
+         * <P>Type: TEXT</P>
+         */
+        public static final String LONGITUDE = "longitude";
+
+        /**
+         * The timestamp for when the note was created
+         * <P>Type: INTEGER (long)</P>
+         */
+        public static final String CREATED_DATE = "created";
+
+        /**
+         * The timestamp for when the note was last modified
+         * <P>Type: INTEGER (long)</P>
+         */
+        public static final String MODIFIED_DATE = "modified";
+    }
+}
