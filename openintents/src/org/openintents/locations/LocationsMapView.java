48d30d3f7be4899e5db3f03afd79176dc011b248
NumBeast <NumBeast@72b678ce-9140-0410-bee8-679b907dd61a>
2008-01-10 02:12:05 +0000

    Added overlay to LocationsMapView Added zoom in/out handlers to LocationsMapview
    git-svn-id: https://openintents.googlecode.com/svn/trunk@233 72b678ce-9140-0410-bee8-679b907dd61a

--- openintents/src/org/openintents/locations/LocationsMapView.java
+++ openintents/src/org/openintents/locations/LocationsMapView.java
@@ -1,6 +1,9 @@
 package org.openintents.locations;
 
+import android.app.NotificationManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
@@ -8,23 +11,48 @@ import com.google.android.maps.MapView;
 import com.google.android.maps.Point;
 
 public class LocationsMapView extends MapActivity {
+
+	private Point point;
+	private MapView view;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        int longitude = 0;
-        int latitude = 0;
         
         Bundle bundle = getIntent().getExtras();
         if( bundle != null){
-        	longitude = bundle.getInteger("longitude");
-        	latitude = bundle.getInteger("latitude");
+        	point = new Point(bundle.getInteger("latitude"), bundle.getInteger("longitude"));
         }
         
-        MapView view = new MapView(this);
+        view = new MapView(this);
         MapController controller = view.getController();
-        controller.centerMapTo(new Point(latitude * 1000000,longitude * 1000000), true); 
+        controller.centerMapTo(new Point(point.getLatitudeE6(),point.getLongitudeE6()), true); 
         controller.zoomTo(9);
-        setContentView(view);  //R.layout.main);       
+        setContentView(view);  //R.layout.main);      
+        
+        view.createOverlayController().add(new LocationsMapOverlay(this), true);
     }
+    
+    public Point getPoint(){
+    	return point;
+    }
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		int level;
+		switch(keyCode){
+			case KeyEvent.KEYCODE_I:
+				// Zoom In
+        		level = view.getZoomLevel();
+				view.getController().zoomTo(level + 1);
+				return true;
+			case KeyEvent.KEYCODE_O:
+	            // Zoom Out
+	            level = view.getZoomLevel();
+	            view.getController().zoomTo(level - 1);
+	            return true;
+		}
+		return false;
+	}    
 }
