1dbf6c7816584f702b062f85e684f2f26bcb464d
friedger <friedger@gmail.com>
2008-01-25 09:01:44 +0000

    add missing pieces for tagging
    git-svn-id: https://openintents.googlecode.com/svn/trunk@326 72b678ce-9140-0410-bee8-679b907dd61a

---
+++ openintents/src/org/openintents/tags/content/ContentBrowserView.java
@@ -0,0 +1,258 @@
+/* 
+ * Copyright (C) 2007-2008 OpenIntents.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openintents.tags.content;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
+import org.openintents.R;
+import org.openintents.main.OpenIntents;
+import org.openintents.provider.Tag.Contents;
+import org.openintents.provider.Tag.Tags;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.ContentURI;
+import android.os.Bundle;
+import android.os.DeadObjectException;
+import android.os.Handler;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.SubMenu;
+import android.view.View;
+import android.view.Menu.Item;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+/**
+ * View to show tags in a hierarchical manner.
+ * 
+ * 
+ * 
+ */
+public class ContentBrowserView extends ListActivity {
+
+	// tag for logging
+	private static final String TAG = "tagHierarchyView";
+	protected static final String ALL = "ALL"; // TODO: Put string into
+	// resource
+	private static final int MENU_ADD_TAG = 1;
+	private static final int MENU_VIEW_CONTENT = 2;
+	private static final int MENU_REMOVE_TAG = 3;
+
+	protected static final int REQUEST_PICK = 1;
+
+	private AutoCompleteTextView mTagFilter;
+
+	private ListAdapter mTaggedContentAdapter;
+	private DirectoryRegister mRegister;
+
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param icicle
+	 *            bundle
+	 */
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		setContentView(R.layout.tags_content_browser);
+
+		getListView();
+
+		mTagFilter = (AutoCompleteTextView) findViewById(R.id.tag_filter);
+
+		mTagFilter.addTextChangedListener(new TextWatcher() {
+
+			public void onTextChanged(CharSequence cs, int a, int b, int c) {
+				fillDataTaggedContent();
+			}
+
+			public void beforeTextChanged(CharSequence cs, int a, int b, int c) {
+				// do nothing
+			}
+		});
+
+		fillDataTagFilter();
+		mRegister = new DirectoryRegister(this);
+
+	}
+
+	/**
+	 * fill data for filter (auto complete).
+	 */
+	private void fillDataTagFilter() {
+		// Get a cursor with all tags
+		Cursor c = getContentResolver().query(Contents.CONTENT_URI,
+				new String[] { Contents._ID, Contents.URI, Contents.TYPE },
+				"type like 'TAG%'", null, Contents.DEFAULT_SORT_ORDER);
+		startManagingCursor(c);
+
+		if (c == null) {
+			Log.e(TAG, "missing tag provider");
+			mTagFilter.setAdapter(new ArrayAdapter(this,
+					android.R.layout.simple_list_item_1,
+					new String[] { "no tag provider" }));
+			return;
+		}
+
+		ArrayList<String> list = new ArrayList<String>();
+		list.add(ALL);
+		while (c.next()) {
+			list.add(c.getString(1));
+		}
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, list);
+		adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
+		mTagFilter.setAdapter(adapter);
+
+	}
+
+	/**
+	 * fill main list.
+	 */
+	private void fillDataTaggedContent() {
+
+		// Get a cursor with all tags
+		Cursor c = getContentResolver().query(Tags.CONTENT_URI,
+				new String[] { Tags._ID, Tags.URI_2 }, "content1.uri like ?",
+				new String[] { mTagFilter.getText().toString() },
+				"content1.uri");
+		startManagingCursor(c);
+
+		if (c == null) {
+			Log.e(TAG, "missing tag provider");
+			setListAdapter(new ArrayAdapter(this,
+					android.R.layout.simple_list_item_1,
+					new String[] { "no tag provider" }));
+			return;
+		}
+
+		ContentListAdapter adapter = new ContentListAdapter(c, this, mRegister);
+		setListAdapter(adapter);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+
+		SubMenu tagMenu = menu.addSubMenu(0, MENU_ADD_TAG,
+				R.string.tags_add_tag);
+		int tagMenuId = 100;
+		for (Directory directory : mRegister.getDirectories().values()) {
+			final Directory dir = directory;
+			Item dirMenuItem = tagMenu.add(0, tagMenuId, dir.name);
+			Intent intent = new Intent(Intent.PICK_ACTION, ContentURI
+					.create(dir.uri));
+			dirMenuItem.setIntent(intent);
+		}
+
+		menu.add(0, MENU_VIEW_CONTENT, R.string.tags_view_content);
+		menu.add(0, MENU_REMOVE_TAG, R.string.tags_remove_tag);
+
+		Intent intent = new Intent(null, Tags.CONTENT_URI);
+		intent.addCategory(Intent.ALTERNATIVE_CATEGORY);
+		menu.addIntentOptions(Menu.ALTERNATIVE, 0, new ComponentName(this,
+				ContentBrowserView.class), null, intent, 0, null);
+
+		intent = new Intent(null, Tags.CONTENT_URI);
+		intent.addCategory(Intent.SELECTED_ALTERNATIVE_CATEGORY);
+		menu.addIntentOptions(Menu.ALTERNATIVE, 0, new ComponentName(this,
+				ContentBrowserView.class), null, intent, 0, null);
+
+		return true;
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		super.onPrepareOptionsMenu(menu);
+
+		boolean show = getListView().getSelectedItem() instanceof Cursor;
+		menu.get(1).setShown(show);
+		menu.get(2).setShown(show);
+		
+		return true;
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, Item item) {
+		super.onMenuItemSelected(featureId, item);
+
+		if (item.getId() >= 100){
+			startSubActivity(item.getIntent(), REQUEST_PICK);
+		}
+		
+		String tag = mTagFilter.getText().toString();
+		Intent intent;
+		switch (item.getId()) {
+		case MENU_ADD_TAG:
+//			Intent intent = new Intent(org.openintents.OpenIntents.TAG_ACTION,
+//					Tags.CONTENT_URI).putExtra(Tags.QUERY_TAG, tag);
+//			startActivity(intent);
+			break;
+		case MENU_VIEW_CONTENT:
+			String uri = ((Cursor) getListView().getSelectedItem())
+					.getString(1);
+			try {
+				intent = new Intent(Intent.VIEW_ACTION, new ContentURI(uri));
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				intent = null;
+			}
+			if (intent != null) {
+				startActivity(intent);
+			}
+			break;
+		case MENU_REMOVE_TAG:
+			uri = ((Cursor) getListView().getSelectedItem()).getString(1);
+			try {
+				getContentResolver().getProvider(Tags.CONTENT_URI).delete(
+						Tags.CONTENT_URI, "uri_1 = ? AND uri_2 = ?",
+						new String[] { tag, uri });
+			} catch (DeadObjectException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		return true;
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode,
+			String data, Bundle extras) {
+		super.onActivityResult(requestCode, resultCode, data, extras);
+
+		String tag = mTagFilter.getText().toString();
+
+		switch (requestCode) {
+		case REQUEST_PICK:
+			Intent intent = new Intent(org.openintents.OpenIntents.TAG_ACTION,
+					Tags.CONTENT_URI).putExtra(Tags.QUERY_TAG, tag).putExtra(
+					Tags.QUERY_URI, data);
+			startActivity(intent);
+		}
+	}
+}
---
+++ openintents/src/org/openintents/tags/content/DirectoryRegister.java
@@ -0,0 +1,218 @@
+package org.openintents.tags.content;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+import org.openintents.R;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import android.content.ContentResolver;
+import android.content.Resources;
+import android.database.Cursor;
+import android.net.ContentURI;
+import android.util.Log;
+
+public class DirectoryRegister {
+
+	private static final String TAG_INTENT = "intent";
+	private static final String TAG_DIR = "dir";
+	private static final String TAG_PACKAGE = "package";
+
+	private static final String ATTR_TEXT_COLUMNS = "text-columns";
+	private static final String ATTR_ID_COLUMN = "id-column";
+	private static final String ATTR_TIME_COLUMN = "time-column";
+	private static final String ATTR_NAME = "name";
+	private static final String ATTR_ACTION = "action";
+	private static final String ATTR_URI = "uri";
+
+	private static final String TAG = "DirectoryRegister";
+
+	private Map<String, Directory> mTables = new HashMap<String, Directory>();
+	private ContentResolver mResolver;
+
+	public DirectoryRegister(android.content.Context context) {
+		mResolver = context.getContentResolver();
+		Resources res = context.getResources();
+		try {
+			fromXML(res.openRawResource(R.raw.browser));
+			fromXML(res.openRawResource(R.raw.contacts));
+			fromXML(res.openRawResource(R.raw.notepad));
+			fromXML(res.openRawResource(R.raw.media));			
+		} catch (Exception e) {
+			e.printStackTrace();			
+		}
+	}
+
+	public void fromXML(InputStream in) throws Exception {
+		XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+
+		XmlPullParser xpp = factory.newPullParser();
+		xpp.setInput(new InputStreamReader(in));
+
+		register(xpp);
+	}
+
+	public void register(XmlPullParser xpp) throws Exception {
+		Stack<Directory> stack = new Stack<Directory>();
+
+		String package_name = null;
+
+		int eventType = xpp.getEventType();
+
+		while (eventType != XmlPullParser.END_DOCUMENT) {
+			String tag = xpp.getName();
+
+			if (eventType == XmlPullParser.START_TAG) {
+				if (tag.equals(TAG_PACKAGE)) {
+					package_name = startPackage(xpp);
+				} else if (tag.equals(TAG_DIR)) {
+					startDirectory(xpp, stack, package_name);
+				} else if (tag.equals(TAG_INTENT)) {
+					startIntent(xpp, stack);
+				}
+			} else if (eventType == XmlPullParser.END_TAG) {
+				if (tag.equals(TAG_DIR)) {
+					endDirectory(stack);
+				}
+			}
+
+			eventType = xpp.next();
+		}
+	}
+
+	private void endDirectory(Stack<Directory> stack) {
+		// do nothing
+	}
+
+	private void startIntent(XmlPullParser xpp, Stack<Directory> stack)
+			throws DirectoryRegisterException {
+		if (stack.size() > 0) {
+			Directory peek = stack.peek();
+			peek.intent_uri = xpp.getAttributeValue(null, ATTR_URI);
+			peek.intent_action = xpp.getAttributeValue(null, ATTR_ACTION);
+		}
+	}
+
+	private void startDirectory(XmlPullParser xpp, Stack<Directory> stack,
+			String package_name) throws DirectoryRegisterException {
+		Directory dir = new Directory();
+		dir.parent_id = (stack.size() > 0) ? stack.peek().id : 0L;
+		dir.package_name = package_name;
+		dir.name = xpp.getAttributeValue(null, ATTR_NAME);
+		dir.text_columns = xpp.getAttributeValue(null, ATTR_TEXT_COLUMNS);
+		dir.id_column = xpp.getAttributeValue(null, ATTR_ID_COLUMN);
+		dir.time_column = xpp.getAttributeValue(null, ATTR_TIME_COLUMN);
+		dir.uri = xpp.getAttributeValue(null, ATTR_URI);
+
+		dir.name = StringUtils.isBlank(dir.name) ? package_name : dir.name;
+		dir.id_column = StringUtils.isBlank(dir.id_column) ? "_id"
+				: dir.id_column;
+
+		notBlank(xpp, ATTR_URI, dir.uri);
+
+		mTables.put(dir.package_name, dir);
+		stack.push(dir);
+	}
+
+	private String startPackage(XmlPullParser xpp)
+			throws DirectoryRegisterException {
+		String package_name = xpp.getAttributeValue(null, ATTR_NAME);
+
+		notBlank(xpp, ATTR_NAME, package_name);
+
+		mTables.remove(package_name);
+		return package_name;
+	}
+
+	private void notBlank(XmlPullParser xpp, String attr, String value)
+			throws DirectoryRegisterException {
+		if (StringUtils.isBlank(value)) {
+			int no = xpp.getLineNumber();
+			throw new DirectoryRegisterException("Line " + no + " : '" + attr
+					+ "' is required.");
+		}
+	}
+
+	public Map<String, Directory> getDirectories() {
+		return mTables;
+	}
+
+	private String[] getProjection(Directory dir) {
+		ArrayList<String> l = new ArrayList<String>();
+		l.add("_id");
+
+		String[] textColumns = dir.getTextColumns();
+
+		for (int i = 0; textColumns != null && i < textColumns.length; i++) {
+			if (StringUtils.isNotBlank(textColumns[i])) {
+				l.add(textColumns[i].trim());
+			}
+		}
+
+		return l.toArray(new String[l.size()]);
+	}
+
+	public String[] getContentBody(ContentURI uri) {
+		Directory dir = getDirectoryForUri(uri.toString());
+		if (dir != null) {
+			String[] projection = getProjection(dir);
+			String orderby;
+
+			if (StringUtils.isNotBlank(dir.time_column)) {
+				orderby = dir.time_column;
+				Log.d(TAG, "orderby=" + dir.time_column);
+			} else {
+				orderby = dir.id_column;
+				Log.d(TAG, "orderby=" + dir.id_column);
+			}	
+			
+			Cursor cursor = mResolver.query(uri, projection,
+					null, null, orderby);
+			int idIndex = cursor.getColumnIndex("_id");
+
+			int[] columnIndex = getColumnIndex(cursor, projection);
+			String[] values = new String[columnIndex.length];
+
+			while (cursor.next()) {
+				String id = cursor.getString(idIndex);
+				getValues(cursor, columnIndex, values);
+			}
+
+			return values;
+		} else {
+			return null;
+		}
+	}
+
+	private void getValues(Cursor cursor, int[] columnIndex, String[] values) {
+		for (int i = 0; i < columnIndex.length; i++) {
+			values[i] = cursor.getString(columnIndex[i]);
+		}
+	}
+
+	private int[] getColumnIndex(Cursor cursor, String[] projection) {
+		int[] index = new int[projection.length];
+		for (int i = 0; i < projection.length; i++) {
+			index[i] = cursor.getColumnIndex(projection[i]);
+		}
+		return index;
+	}
+
+	private Directory getDirectoryForUri(String uri) {
+		Directory result = null;
+		int maxLength = -1;
+		for (Directory dir : mTables.values()) {
+			if (uri.startsWith(dir.uri) && uri.length() > maxLength) {
+				result = dir;
+				maxLength = uri.length();
+			}
+		}
+		return result;
+	}
+
+}
