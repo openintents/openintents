21c9a0d83f79da83b1ed796b0c622b0b0ff1e137
Peli <peli0101@gmail.com>
2008-02-17 16:59:30 +0000

    OpenGLSensors: Split up code from private to public classes.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@413 72b678ce-9140-0410-bee8-679b907dd61a

---
+++ samples/OpenGLSensors/src/org/openintents/samples/openglsensors/Cube.java
@@ -0,0 +1,109 @@
+package org.openintents.samples.openglsensors;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+public class Cube
+{
+    public Cube()
+    {
+        int one = 0x10000;
+        int vertices[] = {
+               -one, -one, -one,
+                one, -one, -one,
+                one,  one, -one,
+               -one,  one, -one,
+               -one, -one,  one,
+                one, -one,  one,
+                one,  one,  one,
+               -one,  one,  one,
+            };
+            
+        int colors[] = {
+                  0,    0,    0,  one,
+                one,    0,    0,  one,
+                one,  one,    0,  one,
+                  0,  one,    0,  one,
+                  0,    0,  one,  one,
+                one,    0,  one,  one,
+                one,  one,  one,  one,
+                  0,  one,  one,  one,
+            };
+
+        byte indices[] = {
+                0, 4, 5,    0, 5, 1,
+                1, 5, 6,    1, 6, 2,
+                2, 6, 7,    2, 7, 3,
+                3, 7, 4,    3, 4, 0,
+                4, 7, 6,    4, 6, 5,
+                3, 0, 1,    3, 1, 2
+        };
+
+	// Buffers to be passed to gl*Pointer() functions
+	// must be direct, i.e., they must be placed on the
+	// native heap where the garbage collector cannot
+	// move them.
+    //
+    // Buffers with multi-byte datatypes (e.g., short, int, float)
+    // must have their byte order set to native order
+
+    ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length*4);
+    vbb.order(ByteOrder.nativeOrder());
+    mVertexBuffer = vbb.asIntBuffer();
+	mVertexBuffer.put(vertices);
+	mVertexBuffer.position(0);
+
+    ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length*4);
+    cbb.order(ByteOrder.nativeOrder());
+	mColorBuffer = cbb.asIntBuffer();
+	mColorBuffer.put(colors);
+	mColorBuffer.position(0);
+
+	mIndexBuffer = ByteBuffer.allocateDirect(indices.length);
+	mIndexBuffer.put(indices);
+	mIndexBuffer.position(0);
+    }
+    
+    public void draw(GL10 gl)
+    {
+        gl.glFrontFace(gl.GL_CW);
+        gl.glVertexPointer(3, gl.GL_FIXED, 0, mVertexBuffer);
+        gl.glColorPointer(4, gl.GL_FIXED, 0, mColorBuffer);
+        gl.glDrawElements(gl.GL_TRIANGLES, 36, gl.GL_UNSIGNED_BYTE, mIndexBuffer);
+    } 
+    
+    public void drawColor(GL10 gl, int red, int green, int blue)
+    {
+    	int one = 0x10000;
+    	one = one * 3/4;
+        int colors[] = {
+        	  red*one, green*one ,  blue*one,  one,
+        	  red*one*2/4, green*one*2/4,  blue*one*2/4,  one,
+        	  red*one, green*one, blue*one,  one,
+        	  red*one*2/4, green*one*2/4,blue*one*2/4,  one,
+        	  red*one, green*one,blue*one,  one,
+        	  red*one*6/8,  green*one*6/8,  blue*one*6/8,  one,
+        	  red*one, green*one, blue*one,  one,
+        	  red*one*6/8,green*one*6/8, blue*one*6/8,  one,
+           };
+
+        ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length*4);
+        cbb.order(ByteOrder.nativeOrder());
+    	mColorBuffer = cbb.asIntBuffer();
+    	mColorBuffer.put(colors);
+    	mColorBuffer.position(0);
+    	
+        gl.glFrontFace(gl.GL_CW);
+        gl.glVertexPointer(3, gl.GL_FIXED, 0, mVertexBuffer);
+        gl.glColorPointer(4, gl.GL_FIXED, 0, mColorBuffer);
+        gl.glDrawElements(gl.GL_TRIANGLES, 36, gl.GL_UNSIGNED_BYTE, mIndexBuffer);
+    } 
+    
+    
+    private IntBuffer   mVertexBuffer;
+    private IntBuffer   mColorBuffer;
+    private ByteBuffer  mIndexBuffer;
+}
---
+++ samples/OpenGLSensors/src/org/openintents/samples/openglsensors/Pyramid.java
@@ -0,0 +1,91 @@
+package org.openintents.samples.openglsensors;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
+//The pyramid is taken from the anddev.org tutorial.
+
+public class Pyramid { 
+      
+     private IntBuffer mVertexBuffer; 
+     private IntBuffer mColorBuffer; 
+     private ByteBuffer mIndexBuffer; 
+
+     public Pyramid() { 
+                     
+          int one = 0x10000; 
+          /* Every vertex got 3 values, for 
+          * x / y / z position in the kartesian space. 
+          */ 
+          int vertices[] = { -one, -one, -one, // The four floor vertices of the pyramid 
+               one, -one, -one, 
+               one, one, -one, 
+               -one, one, -one, 
+               0, 0, one, };  // The top of the pyramid 
+
+          /* Every vertex has got its own color, described by 4 values 
+          * R(ed) 
+          * G(green) 
+          * B(blue) 
+          * A(lpha) <-- Opacity 
+          */ 
+          int colors[] = { 0, 0, one, one, 
+               one, 0, 0, one, 
+               one, one, 0, one, 
+               0, one, 0, one, 
+               one, 0, one, one, }; 
+
+           /* The last thing is that we need to describe some Triangles. 
+           * A triangle got 3 vertices. 
+           * It is important in which order 
+           * the vertices of each triangle are described. 
+           * So describing a triangle through the vertices: "0, 3, 4" 
+           * will not result in the same triangle as: "0, 4, 3" 
+           * The reason for that is the call of: "gl.glFrontFace(gl.GL_CW);" 
+           * which means, that we have to describe the "visible" side of the 
+           * triangles by naming its vertices in a ClockWise order! 
+           * From the other side, the triangle will be 100% transparent! 
+           */ 
+          byte indices[] = { 0, 4, 1, // The four side-triangles 
+               1, 4, 2, 
+               2, 4, 3, 
+               3, 4, 0, 
+               1, 2, 0, // The two bottom-triangles 
+               0, 2, 3}; 
+
+          // Buffers to be passed to gl*Pointer() functions 
+          // must be direct, i.e., they must be placed on the 
+          // native heap where the garbage collector cannot 
+          // move them. 
+          // 
+          // Buffers with multi-byte datatypes (e.g., short, int, float) 
+          // must have their byte order set to native order 
+
+          ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);  // * 4 becuase of int 
+          vbb.order(ByteOrder.nativeOrder()); 
+          mVertexBuffer = vbb.asIntBuffer(); 
+          mVertexBuffer.put(vertices); 
+          mVertexBuffer.position(0); 
+
+          ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length * 4); // * 4 becuase of int 
+          cbb.order(ByteOrder.nativeOrder()); 
+          mColorBuffer = cbb.asIntBuffer(); 
+          mColorBuffer.put(colors); 
+          mColorBuffer.position(0); 
+
+          mIndexBuffer = ByteBuffer.allocateDirect(indices.length); 
+          mIndexBuffer.put(indices); 
+          mIndexBuffer.position(0); 
+     } 
+
+     public void draw(GL10 gl) { 
+          gl.glFrontFace(gl.GL_CW); 
+          gl.glVertexPointer(3, gl.GL_FIXED, 0, mVertexBuffer); 
+          gl.glColorPointer(4, gl.GL_FIXED, 0, mColorBuffer); 
+          gl.glDrawElements(gl.GL_TRIANGLES, 18, gl.GL_UNSIGNED_BYTE, mIndexBuffer); 
+     } 
+
+}
