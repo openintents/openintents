310ba0f1a30dd0203d1b58f302780f699b9fa8d8
zeroogle <zeroogle@gmail.com>
2008-03-21 22:49:43 +0000

    Alert Framework : KISS 0.1
    git-svn-id: https://openintents.googlecode.com/svn/trunk@753 72b678ce-9140-0410-bee8-679b907dd61a

--- openintents/src/org/openintents/provider/Alert.java
+++ openintents/src/org/openintents/provider/Alert.java
@@ -1,5 +1,22 @@
 package org.openintents.provider;
 
+/* 
+ * Copyright (C) 2007-2008 OpenIntents.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.net.Uri;
@@ -46,6 +63,12 @@ public class Alert{
 
 		public static final String INTENT="intent";
 
+		public static final String INTENT_CATEGORY="intent_category";
+
+		public static final String INTENT_URI="intent_uri";
+
+		public static final String DEFAULT_SORT_ORDER="";
+
 		public static final String[] PROJECTION={
 			_ID,
 			_COUNT,
@@ -56,7 +79,9 @@ public class Alert{
 			NATURE,
 			ACTIVE,
 			ACTIVATE_ON_BOOT,
-			INTENT
+			INTENT,
+			INTENT_CATEGORY,
+			INTENT_URI
 		};
 
 	};
@@ -82,7 +107,13 @@ public class Alert{
 		public static final String ACTIVATE_ON_BOOT=Generic.ACTIVATE_ON_BOOT;
 
 		public static final String INTENT=Generic.INTENT;
+
+		public static final String INTENT_CATEGORY=Generic.INTENT_CATEGORY;
+
+		public static final String INTENT_URI=Generic.INTENT_URI;
 		
+		public static final String DEFAULT_SORT_ORDER="";
+
 		public static final String[] PROJECTION={
 			_ID,
 			_COUNT,
@@ -93,9 +124,10 @@ public class Alert{
 			NATURE,
 			ACTIVE,
 			ACTIVATE_ON_BOOT,
-			INTENT
-		};
-			
+			INTENT,
+			INTENT_CATEGORY,
+			INTENT_URI
+		};			
 
 	};
 
