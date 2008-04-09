6f3323f52f613de02e1864c615590d18d9ba8441
Peli <peli0101@gmail.com>
2008-04-09 05:52:35 +0000

    ShoppingList: Add location alert: functionality added (issue #11).
    git-svn-id: https://openintents.googlecode.com/svn/trunk@910 72b678ce-9140-0410-bee8-679b907dd61a

--- openintents/src/org/openintents/shopping/AddLocationAlertActivity.java
+++ openintents/src/org/openintents/shopping/AddLocationAlertActivity.java
@@ -17,17 +17,21 @@
 package org.openintents.shopping;
 
 import org.openintents.R;
-import org.openintents.provider.Shopping;
+import org.openintents.alert.AlertList;
+import org.openintents.provider.Alert;
+import org.openintents.provider.Location.Locations;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -40,10 +44,14 @@ public class AddLocationAlertActivity extends Activity
      */
     private static final String TAG = "AddLocationAlertActivity";
     
+    private static final int REQUEST_PICK_LOC = 1;
+	
+    
+    TextView mAlertAdded;
     TextView mTags;
     TextView mLocation;
     
-    Uri mUri;
+    Uri mShoppingListUri;
     
     @Override
     public void onCreate(Bundle icicle) {
@@ -52,18 +60,21 @@ public class AddLocationAlertActivity extends Activity
         setContentView(R.layout.shopping_add_location_alert);
 
         // Get the uri of the list
-        mUri = getIntent().getData();
+        mShoppingListUri = getIntent().getData();
 
         // Set up click handlers for the text field and button
+        mAlertAdded = (TextView) this.findViewById(R.id.alert_added_text);
         mTags = (TextView) this.findViewById(R.id.tags);
         mLocation = (TextView) this.findViewById(R.id.location);
         
         Button picklocation = (Button) this.findViewById(R.id.picklocation);
         picklocation.setOnClickListener(this);
 
+        /*
         Button addlocationalert = (Button) this.findViewById(R.id.addlocationalert);
         addlocationalert.setOnClickListener(this);
-
+         */
+        
         Button viewalerts = (Button) this.findViewById(R.id.viewalerts);
         viewalerts.setOnClickListener(this);
 
@@ -75,9 +86,9 @@ public class AddLocationAlertActivity extends Activity
         case R.id.picklocation:
         	pickLocation();
         	break;
-        case R.id.addlocationalert:
-        	addLocationAlert();
-        	break;
+        //case R.id.addlocationalert:
+        //	addLocationAlert();
+        //	break;
         case R.id.viewalerts:
         	viewAlerts();
         	break;
@@ -102,15 +113,71 @@ public class AddLocationAlertActivity extends Activity
     }
 
     public void pickLocation() {
-    	
+    	// Call the pick location activity
+    	Intent intent;
+		
+    	intent = new Intent(Intent.PICK_ACTION, Locations.CONTENT_URI);
+		startSubActivity(intent, REQUEST_PICK_LOC);
     }
     
     public void addLocationAlert() {
-    	
+    	// Add location into database
+    	addAlert(mLocation.getText().toString(), null, Intent.VIEW_ACTION,
+    			null, mShoppingListUri.getPath());
     }
     
     public void viewAlerts() {
-    	
+    	// View list of alerts
+
+    	Intent intent = new Intent(AddLocationAlertActivity.this, AlertList.class);
+		startActivity(intent);
     }
+    
+    /// TODO: Simply copied this routine from LocationsView.java.
+    // This should be a convenience function in the alerts provider!
+    private void addAlert(String locationUri, String data, String actionName,
+			String type, String uri) {
+
+		ContentValues values = new ContentValues();
+		values.put(Alert.Location.ACTIVE, Boolean.TRUE);
+		values.put(Alert.Location.ACTIVATE_ON_BOOT, Boolean.TRUE);
+		values.put(Alert.Location.DISTANCE, 100L);
+		values.put(Alert.Location.POSITION, locationUri);
+		values.put(Alert.Location.INTENT, actionName);
+		values.put(Alert.Location.INTENT_URI, uri);
+		// TODO convert type to uri (?) or add INTENT_MIME_TYPE column
+		// getContentResolver().insert(Alert.Location.CONTENT_URI, values);
+		// using alert.insert will register alerts automatically.
+		Uri result = Alert.insert(Alert.Location.CONTENT_URI, values);
+		int textId; 
+		if (uri != null){
+			textId = R.string.alert_added;
+			
+			mAlertAdded.setText(getString(R.string.location_alert_added));
+		} else {
+			textId = R.string.alert_not_added;
+			
+			mAlertAdded.setText(getString(R.string.alert_not_added));
+		}
+		Toast.makeText(this, textId, Toast.LENGTH_SHORT)
+		.show();
+
+
+	}
+    
+    
+    @Override
+	protected void onActivityResult(int requestCode, int resultCode, String data,
+			Bundle bundle) {
+		if (resultCode == RESULT_OK) {
+			switch (requestCode) {
+			
+			case REQUEST_PICK_LOC:
+				mLocation.setText(bundle.getString(Locations.EXTRA_GEO));				
+				addLocationAlert();
+				break;
+			}
+		}
+	}
     	
 }
