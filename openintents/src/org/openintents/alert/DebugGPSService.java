eb7c9e849defdea3aca799fd9917c12c5cacb8b2
friedger <friedger@gmail.com>
2008-04-09 00:26:40 +0000

    write tracked route to file
    git-svn-id: https://openintents.googlecode.com/svn/trunk@900 72b678ce-9140-0410-bee8-679b907dd61a

--- openintents/src/org/openintents/alert/DebugGPSService.java
+++ openintents/src/org/openintents/alert/DebugGPSService.java
@@ -1,5 +1,10 @@
 package org.openintents.alert;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 import android.app.NotificationManager;
 import android.app.Notification;
 import android.app.Service;
@@ -7,6 +12,7 @@ import android.os.IBinder;
 import android.os.Parcel;
 import android.util.Log;
 import android.content.ContentUris;
+import android.content.Context;
 import android.database.Cursor;
 import android.widget.Toast;
 import android.location.LocationManager;
@@ -15,6 +21,7 @@ import android.location.Location;
 import android.os.Looper;
 import android.os.Handler;
 import android.os.Message;
+
 /* 
  * Copyright (C) 2007-2008 OpenIntents.org
  *
@@ -32,81 +39,109 @@ import android.os.Message;
  */
 /**
  * This class will print the current location to the logfile every 30seconds.
- * DON'T USE THIS ON A REAL DEVICE!
- * your battery will drain in now time! you have been warned! ;)
- * This is for debugging purposes only
+ * DON'T USE THIS ON A REAL DEVICE! your battery will drain in now time! you
+ * have been warned! ;) This is for debugging purposes only
  */
-public class DebugGPSService  extends Service implements Runnable{
+public class DebugGPSService extends Service implements Runnable {
 
-	private boolean alive=false;
+	private boolean alive = false;
 
-	private static final String _TAG="DebugGPSService";
+	private static final String _TAG = "DebugGPSService";
 	LocationManager locMan;
-    protected void onCreate() {
 
-        // Start up the thread running the service.  Note that we create a
-        // separate thread because the service normally runs in the process's
+	protected void onCreate() {
+
+		// Start up the thread running the service. Note that we create a
+		// separate thread because the service normally runs in the process's
 		// main thread, which we don't want to block.
-		Toast.makeText(this, "DebugGPSService started", Toast.LENGTH_SHORT).show();
-		
-		locMan=(LocationManager)getSystemService(android.content.Context.LOCATION_SERVICE);
-        this.alive=true;
-        Thread thr = new Thread(null, this, _TAG);
-        thr.start();
-    }
+		Toast.makeText(this, "DebugGPSService started", Toast.LENGTH_SHORT)
+				.show();
 
+		locMan = (LocationManager) getSystemService(android.content.Context.LOCATION_SERVICE);
+		this.alive = true;
+		Thread thr = new Thread(null, this, _TAG);
+		thr.start();
+	}
 
-	public void run() {		
-		  //Log.d(_TAG,"BEFORE LOOPER BLOCK");
-          Looper.prepare();
-          
-         Handler mHandler = new Handler() {
-              public void handleMessage(Message msg) {
-                  // process incoming messages here
-              }
-          };
-          
-        //  Looper.loop();
-		//  Log.d(_TAG,"AFERT LOOPER BLOCK");
-		while (this.alive){
-			
-			try{
-				Thread.sleep(30*1000);
+	public void run() {
+		// Log.d(_TAG,"BEFORE LOOPER BLOCK");
+		Looper.prepare();
 
-				Location l=locMan.getCurrentLocation("gps");
-				if (l==null)
-				{
-					//Toast.makeText(this, "Current Position>> null <<", Toast.LENGTH_SHORT).show();
-					Log.d(_TAG,"Current Position>> null <<");
-				}else{
-					//Toast.makeText(this, "Current Position>>"+l.getLatitude()+":"+l.getLongitude()+"<<", Toast.LENGTH_SHORT).show();
-					Log.d(_TAG,"Current Position>>"+l.getLatitude()+":"+l.getLongitude()+"<<");
+		Handler mHandler = new Handler() {
+			public void handleMessage(Message msg) {
+				// process incoming messages here
+			}
+		};
+
+		FileOutputStream fos = null;
+		try {
+			fos = openFileOutput("locations.sql", Context.MODE_WORLD_READABLE
+					& Context.MODE_WORLD_WRITEABLE);
+		} catch (FileNotFoundException e1) {
+			Log.e(_TAG, e1.getMessage(), e1);
+		}
+		OutputStreamWriter writer = new OutputStreamWriter(fos);
+
+		// Looper.loop();
+		// Log.d(_TAG,"AFERT LOOPER BLOCK");
+		while (this.alive) {
+
+			try {
+				Thread.sleep(30 * 1000);
+
+				Location l = locMan.getCurrentLocation("gps");
+				if (l == null) {
+					// Toast.makeText(this, "Current Position>> null <<",
+					// Toast.LENGTH_SHORT).show();
+					Log.d(_TAG, "Current Position>> null <<");
+				} else {
+					// Toast.makeText(this, "Current
+					// Position>>"+l.getLatitude()+":"+l.getLongitude()+"<<",
+					// Toast.LENGTH_SHORT).show();
+					Log.d(_TAG, "Current Position>>" + l.getLatitude() + ":"
+							+ l.getLongitude() + "<<");
+
+					try {
+						writer
+								.append(String
+										.format(
+												"INSERT INTO locations (latitude, longitude) VALUES ( %f, %f);\n",
+												l.getLatitude(),
+												l.getLongitude()));
+					} catch (IOException e) {
+						Log.e(_TAG, e.getMessage(), e);
+					}
 				}
-			}catch(Exception e){   
-				
-				Log.e(_TAG,"Error:"+e.getMessage());
+			} catch (Exception e) {
+
+				Log.e(_TAG, "Error:" + e.getMessage());
 				e.printStackTrace();
 			}
-			
+
 		}
-		//finished/stopp called. cleanup & exit
+		try {
+			writer.close();
+		} catch (IOException e) {
+			Log.e(_TAG, e.getMessage(), e);
+		}
+		// finished/stopp called. cleanup & exit
 		this.cleanup();
 	}
 
-	
-	private void cleanup(){
-		Log.d(_TAG,"Cleaning up...");
+	private void cleanup() {
+		Log.d(_TAG, "Cleaning up...");
 	}
 
-	public IBinder onBind(android.content.Intent i){
+	public IBinder onBind(android.content.Intent i) {
 		return null;
 	}
 
 	@Override
-	public void onDestroy(){
-		Toast.makeText(this, _TAG+" stoping..", Toast.LENGTH_SHORT).show();
-		this.alive=false;
-	//	mNM.notifyWithText(1, "thread stopping", NotificationManager.LENGTH_SHORT,null);
+	public void onDestroy() {
+		Toast.makeText(this, _TAG + " stoping..", Toast.LENGTH_SHORT).show();
+		this.alive = false;
+		// mNM.notifyWithText(1, "thread stopping",
+		// NotificationManager.LENGTH_SHORT,null);
 	}
 
 }
