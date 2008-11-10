85c6a8ed1e6bc3531126a62181463e56601989d0
Peli <peli0101@gmail.com>
2008-11-10 22:51:13 +0000

    Show ignore icon also when only current version is ignored.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@1341 72b678ce-9140-0410-bee8-679b907dd61a

--- UpdateChecker/src/org/openintents/updatechecker/activity/UpdateListCursorAdapter.java
+++ UpdateChecker/src/org/openintents/updatechecker/activity/UpdateListCursorAdapter.java
@@ -2,16 +2,17 @@ package org.openintents.updatechecker.activity;
 
 import org.openintents.updatechecker.AppListInfo;
 import org.openintents.updatechecker.OpenMatrixCursor;
-import org.openintents.updatechecker.UpdateInfo;
 
 import android.content.Context;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 
 public class UpdateListCursorAdapter extends CursorAdapter {
+	private static final String TAG = "UpdateListCursorAdapter";
 
 
 	Context mContext;
@@ -39,6 +40,20 @@ public class UpdateListCursorAdapter extends CursorAdapter {
 		
 		int no_notifications = cursor.getInt(cursor
 				.getColumnIndexOrThrow(AppListInfo.NO_NOTIFICATIONS));
+		
+		int versioncode = cursor.getInt(cursor
+				.getColumnIndexOrThrow(AppListInfo.VERSION_CODE));
+		int latestversioncode = cursor.getInt(cursor
+				.getColumnIndexOrThrow(AppListInfo.LATEST_VERSION_CODE));
+		int ignoreversioncode = cursor.getInt(cursor
+				.getColumnIndexOrThrow(AppListInfo.IGNORE_VERSION_CODE));
+		
+		Log.i(TAG, name + " version: " + latestversioncode + ", " + versioncode + ", " + ignoreversioncode);
+		
+		if (ignoreversioncode > versioncode) {
+			// Also show "Ignore" sign, if current version is being ignored.
+			no_notifications = 1;
+		}
 		
 		cliv.setName(name);
 		cliv.setInfo(info);
