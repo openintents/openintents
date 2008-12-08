000803e7c089f315247210d493915eee2e251254
Peli <peli0101@gmail.com>
2008-12-08 10:19:19 +0000

    Set cursor to last position.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@1531 72b678ce-9140-0410-bee8-679b907dd61a

--- FileManager/src/org/openintents/filemanager/FileManagerActivity.java
+++ FileManager/src/org/openintents/filemanager/FileManagerActivity.java
@@ -232,7 +232,11 @@ public class FileManagerActivity extends ListActivity {
  	private void refreshDirectoryPanel() {
  		if (isDirectoryInputVisible()) {
  			// Set directory path
- 			mEditDirectory.setText(currentDirectory.getAbsolutePath());
+ 			String path = currentDirectory.getAbsolutePath();
+ 			mEditDirectory.setText(path);
+ 			
+ 			// Set selection to last position so user can continue to type:
+ 			mEditDirectory.setSelection(path.length());
  		} else {
  			setDirectoryButtons();
  		}
