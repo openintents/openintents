0ac4a66b8b3731848714a1a72d7c3671e5c40739
Peli <peli0101@gmail.com>
2008-12-09 00:46:24 +0000

    Allow button to have custom text.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@1538 72b678ce-9140-0410-bee8-679b907dd61a

--- FileManager/src/org/openintents/filemanager/FileManagerActivity.java
+++ FileManager/src/org/openintents/filemanager/FileManagerActivity.java
@@ -1,3 +1,23 @@
+/* 
+ * Copyright (C) 2008 OpenIntents.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ * Based on AndDev.org's file browser V 2.0.
+ */
+
 package org.openintents.filemanager;
 
 import java.io.File;
@@ -163,6 +183,11 @@ public class FileManagerActivity extends ListActivity {
         	  String title = intent.getStringExtra(FileManagerIntents.EXTRA_TITLE);
         	  if (title != null) {
         		  setTitle(title);
+        	  }
+
+        	  String buttontext = intent.getStringExtra(FileManagerIntents.EXTRA_BUTTON_TEXT);
+        	  if (buttontext != null) {
+        		  mButtonPick.setText(buttontext);
         	  }
           } else {
         	  mState = STATE_BROWSE;
