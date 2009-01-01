b69ecb9efd5f47a97f017ed305f76859adfc16cf
friedger <friedger@gmail.com>
2009-01-01 16:00:56 +0000

    Dummy App
    git-svn-id: https://openintents.googlecode.com/svn/trunk@1610 72b678ce-9140-0410-bee8-679b907dd61a

---
+++ AboutApp/src/org/openintents/about/AboutActivity.java
@@ -0,0 +1,13 @@
+package org.openintents.about;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
