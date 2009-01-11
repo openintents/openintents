8355b609248e00ea74bb4d9be850b6a12b6482df
Peli <peli0101@gmail.com>
2009-01-11 12:51:47 +0000

    Change order of menu items (tags before encrypt).
    git-svn-id: https://openintents.googlecode.com/svn/trunk@1689 72b678ce-9140-0410-bee8-679b907dd61a

--- NotePad/src/org/openintents/notepad/noteslist/NotesList.java
+++ NotePad/src/org/openintents/notepad/noteslist/NotesList.java
@@ -384,15 +384,14 @@ public class NotesList extends ListActivity implements ListView.OnScrollListener
 		// Add a menu item to send the note
 		menu.add(0, MENU_ITEM_SEND_BY_EMAIL, 0, R.string.menu_send_by_email);
 
+		menu.add(0, MENU_ITEM_EDIT_TAGS, 0, R.string.menu_edit_tags);
+		
 		long encrypted = cursor.getLong(NotesListCursor.COLUMN_INDEX_ENCRYPTED);
 		if (encrypted <= 0) {
 			menu.add(0, MENU_ITEM_ENCRYPT, 0, R.string.menu_encrypt);
 		} else {
 			menu.add(0, MENU_ITEM_UNENCRYPT, 0, R.string.menu_undo_encryption);
 		}
-			
-
-		menu.add(0, MENU_ITEM_EDIT_TAGS, 0, R.string.menu_edit_tags);
 		
 		// Add a menu item to delete the note
 		menu.add(0, MENU_ITEM_DELETE, 0, R.string.menu_delete);
