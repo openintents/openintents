d5aff09d509275edfd7912178fde7c2e107d2ad9
Peli <peli0101@gmail.com>
2009-01-17 07:18:08 +0000

    Remove unnecessary resources.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@1744 72b678ce-9140-0410-bee8-679b907dd61a

--- FileManager/src/org/openintents/distribution/RD.java
+++ FileManager/src/org/openintents/distribution/RD.java
@@ -11,28 +11,19 @@ import org.openintents.filemanager.R;
  */
 public class RD {
 	public class layout {
-		public static final int about = R.layout.about;
 		public static final int eula = R.layout.eula;
 	}
 	
-	public class id {
-		///////////////////////////////////////
-		// About activity
-		public static final int text = R.id.text;
-		
+	public class id {		
 		///////////////////////////////////////
 		// Eula activity
+		public static final int text = R.id.text;
 		public static final int button1 = R.id.button1;
 		public static final int button2 = R.id.button2;
 		
 	}
 	
-	public class string {
-		///////////////////////////////////////
-		// About activity
-		public static final int about_title = R.string.about_title;
-		public static final int about_text = R.string.about_text;
-		
+	public class string {		
 		///////////////////////////////////////
 		// Eula activity
 
