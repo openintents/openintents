fc86996559c5c579b390aaa9362b871da4d7ab74
Peli <peli0101@gmail.com>
2009-06-20 21:06:56 +0000

    OI Notepad: Import translations from Launchpad.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@2167 72b678ce-9140-0410-bee8-679b907dd61a

--- NotePad/translations/androidxml2po.bash
+++ NotePad/translations/androidxml2po.bash
@@ -4,9 +4,9 @@
 #Copyright 2009 by pjv. Licensed under GPLv3.
 
 #Set the languages here (long version is the android resource append string).
-short_lang=("de" "es" "fr" "nl") #do not include template language ("en" usually).
+short_lang=("de" "es" "fr" "lo" "nl") #do not include template language ("en" usually).
 #="nl" "de" "fr" "ar" "es" "he" "hu" "id" "it" "pl" "pt_BR" "ru" "sv" "zh_CN"
-long_lang=("de" "es" "fr" "nl") #do not include template language ("en" usually).
+long_lang=("de" "es" "fr" "lo" "nl") #do not include template language ("en" usually).
 # "nl" "de" "fr" "ar" "es" "he" "hu" "id" "it" "pl" "pt-rBR" "ru" "sv" "zh-rCN"
 #Change the dirs where the files are located. Dirs cannot have leading "."'s or msgmerge will complain.
 launchpad_po_files_dir="."
