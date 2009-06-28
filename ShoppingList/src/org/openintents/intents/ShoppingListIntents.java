ad9d959e97f843d746a312d8b969d35f50380faf
Peli <peli0101@gmail.com>
2009-06-28 01:16:58 +0000

    OI Shopping list: Add automation task to clean up shopping list.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@2194 72b678ce-9140-0410-bee8-679b907dd61a

---
+++ ShoppingList/src/org/openintents/intents/ShoppingListIntents.java
@@ -0,0 +1,31 @@
+package org.openintents.intents;
+
+/**
+ * 
+ * @author Peli
+ * @version 1.1.0
+ */
+public class ShoppingListIntents {
+
+	/**
+	 * String extra containing the action to be performed.
+	 * 
+	 * <p>Constant Value: "org.openintents.extra.ACTION"</p>
+	 */
+	public static final String EXTRA_ACTION = "org.openintents.extra.ACTION";
+
+	/**
+	 * String extra containing the data on which to perform the action.
+	 * 
+	 * <p>Constant Value: "org.openintents.extra.DATA"</p>
+	 */
+	public static final String EXTRA_DATA = "org.openintents.extra.DATA";
+
+	/**
+	 * Task to be used in EXTRA_ACTION.
+	 * 
+	 * <p>Constant Value: "CLEAN_UP_LIST"</p>
+	 */
+	public static final String TASK_CLEAN_UP_LIST = "clean_up_list";
+	
+}
