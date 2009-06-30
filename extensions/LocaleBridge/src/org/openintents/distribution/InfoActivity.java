e1deab52935093f50491ce9589b31a05cda1b22f
Peli <peli0101@gmail.com>
2009-06-30 20:57:53 +0000

    OI Locale Bridge: Add license information.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@2215 72b678ce-9140-0410-bee8-679b907dd61a

--- extensions/LocaleBridge/src/org/openintents/distribution/InfoActivity.java
+++ extensions/LocaleBridge/src/org/openintents/distribution/InfoActivity.java
@@ -1,6 +1,20 @@
-package org.openintents.distribution;
+/* 
+ * Copyright (C) 2008-2009 OpenIntents.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.util.List;
+package org.openintents.distribution;
 
 import org.openintents.localebridge.R;
 import org.openintents.util.IntentUtils;
@@ -11,8 +25,6 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.DialogInterface.OnDismissListener;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.util.Log;
 
