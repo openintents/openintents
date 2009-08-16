48aa760a49df96a8803ba87fd751a19c1972d6f4
ebomike <ebomike@gmail.com>
2009-08-16 05:58:21 +0000

    Use Webkit's map to convert from extension to MIME type before consulting the own XML map.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@2277 72b678ce-9140-0410-bee8-679b907dd61a

--- FileManager/src/org/openintents/filemanager/util/MimeTypes.java
+++ FileManager/src/org/openintents/filemanager/util/MimeTypes.java
@@ -19,6 +19,8 @@ package org.openintents.filemanager.util;
 import java.util.HashMap;
 import java.util.Map;
 
+import android.webkit.MimeTypeMap;
+
 public class MimeTypes {
 
 	private Map<String, String> mMimeTypes;
@@ -37,6 +39,17 @@ public class MimeTypes {
 	public String getMimeType(String filename) {
 		
 		String extension = FileUtils.getExtension(filename);
+		
+		// Let's check the official map first. Webkit has a nice extension-to-MIME map.
+		// Be sure to remove the first character from the extension, which is the "." character.
+		if (extension.length() > 0) {
+			String webkitMimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension.substring(1));
+		
+			if (webkitMimeType != null) {
+				// Found one. Let's take it!
+				return webkitMimeType;
+			}
+		}
 		
 		// Convert extensions to lower case letters for easier comparison
 		extension = extension.toLowerCase();
