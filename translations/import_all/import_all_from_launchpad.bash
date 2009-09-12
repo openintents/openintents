7771469b3065979dbf19b44a7363c7bc787f91ed
Peli <peli0101@gmail.com>
2009-09-12 20:24:54 +0000

    Add translation import scripts.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@2299 72b678ce-9140-0410-bee8-679b907dd61a

---
+++ translations/import_all/import_all_from_launchpad.bash
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "Extracing translation files..."
+tar -xvvzf launchpad-export.tar.gz
+
+echo "Translating AboutApp"
+cp translations_aboutapp/* ../../AboutApp/translations
+cd ../../AboutApp/translations
+androidxml2po.bash -i
+cd ../../translations/import_all
+
+echo "Translating Countdown"
+cp translations_countdown/* ../../Countdown/translations
+cd ../../Countdown/translations
+androidxml2po.bash -i
+cd ../../translations/import_all
+
+echo "Translating ShoppingList"
+cp translations_filemanager/* ../../ShoppingList/translations
+cd ../../ShoppingList/translations
+androidxml2po.bash -i
+cd ../../translations/import_all
+
+echo "Translating Flashlight"
+cp flashlight/* ../../Flashlight/translations
+cp translations_flashlight/* ../../Flashlight/translations
+cd ../../Flashlight/translations
+androidxml2po.bash -i
+cd ../../translations/import_all
+
+echo "Translating NotePad"
+cp translations_notepad/* ../../NotePad/translations
+cd ../../NotePad/translations
+androidxml2po.bash -i
+cd ../../translations/import_all
+
+echo "Translating Safe"
+cp translations_safe/* ../../Safe/translations
+cd ../../Safe/translations
+androidxml2po.bash -i
+cd ../../translations/import_all
+
+echo "Translating ShoppingList"
+cp translations_shoppinglist/* ../../ShoppingList/translations
+cd ../../ShoppingList/translations
+androidxml2po.bash -i
+cd ../../translations/import_all
+
+echo "Translating UpdateChecker"
+cp translations_updatechecker/* ../../UpdateChecker/translations
+cd ../../UpdateChecker/translations
+androidxml2po.bash -i
+cd ../../translations/import_all
---
+++ translations/import_all/import_all_from_launchpad_cygwin.bat
@@ -0,0 +1,4 @@
+set PATH=.;c:\cygwin\bin;%PATH%
+bash import_all_from_launchpad.bash
+
+pause
