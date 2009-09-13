15fd588554c4164635ba696aaffc0a9c056dab17
Peli <peli0101@gmail.com>
2009-09-13 00:40:26 +0000

    Fix export translation script.
    git-svn-id: https://openintents.googlecode.com/svn/trunk@2304 72b678ce-9140-0410-bee8-679b907dd61a

--- translations/export_all/export_all_to_launchpad.bash
+++ translations/export_all/export_all_to_launchpad.bash
@@ -4,58 +4,58 @@
 echo "Translating AboutApp"
 mkdir translations_aboutapp
 cd ../../AboutApp/translations
-androidxml2po.bash -i
-cp *.pot *.po ../../translations/export_all/translations_aboutapp
+androidxml2po.bash -e
+cp *.pot ../../translations/export_all/translations_aboutapp
 cd ../../translations/export_all
 
 echo "Translating Countdown"
 mkdir translations_countdown
 cd ../../Countdown/translations
-androidxml2po.bash -i
-cp *.pot *.po ../../translations/export_all/translations_countdown
+androidxml2po.bash -e
+cp *.pot ../../translations/export_all/translations_countdown
 cd ../../translations/export_all
 
 echo "Translating FileManager"
 mkdir translations_filemanager
 cd ../../FileManager/translations
-androidxml2po.bash -i
-cp *.pot *.po ../../translations/export_all/translations_filemanager
+androidxml2po.bash -e
+cp *.pot ../../translations/export_all/translations_filemanager
 cd ../../translations/export_all
 
 echo "Translating Flashlight"
 mkdir translations_flashlight
 cd ../../Flashlight/translations
-androidxml2po.bash -i
-cp *.pot *.po ../../translations/export_all/translations_flashlight
+androidxml2po.bash -e
+cp *.pot ../../translations/export_all/translations_flashlight
 cd ../../translations/export_all
 
 echo "Translating NotePad"
 mkdir translations_notepad
 cd ../../NotePad/translations
-androidxml2po.bash -i
-cp *.pot *.po ../../translations/export_all/translations_notepad
+androidxml2po.bash -e
+cp *.pot ../../translations/export_all/translations_notepad
 cd ../../translations/export_all
 
 echo "Translating Safe"
 mkdir translations_safe
 cd ../../Safe/translations
-androidxml2po.bash -i
-cp *.pot *.po ../../translations/export_all/translations_safe
+androidxml2po.bash -e
+cp *.pot ../../translations/export_all/translations_safe
 cd ../../translations/export_all
 
 echo "Translating ShoppingList"
 mkdir translations_shoppinglist
 cd ../../ShoppingList/translations
-androidxml2po.bash -i
-cp *.pot *.po ../../translations/export_all/translations_shoppinglist
+androidxml2po.bash -e
+cp *.pot ../../translations/export_all/translations_shoppinglist
 cd ../../translations/export_all
 
 echo "Translating UpdateChecker"
 mkdir translations_updatechecker
 cd ../../UpdateChecker/translations
-androidxml2po.bash -i
-cp *.pot *.po ../../translations/export_all/translations_updatechecker
+androidxml2po.bash -e
+cp *.pot ../../translations/export_all/translations_updatechecker
 cd ../../translations/export_all
 
 echo "Creating tar.gz file for upload..."
-tar -cvvzf launchpad-export.tar.gz translations_*
+tar -cvvzf launchpad-upload.tar.gz translations_*
