openintents/openintents

commit aec964f3f51dc648db2835de19171558230da12e

OI Notepad: Apply patch by matthiasblaesing1, see issue #241: Offer tag selection on the main screen
git-svn-id: https://openintents.googlecode.com/svn/trunk@2430 72b678ce-9140-0410-bee8-679b907dd61a

NotePad/src/org/openintents/notepad/noteslist/NotesList.java
@@ -60,6 +60,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.Cursor;
+import android.database.DataSetObserver;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -76,6 +77,9 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.AbsListView.OnScrollListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import java.util.List;
 
 /**
  * Displays a list of notes. Will display notes from the {@link Uri} provided in
@@ -123,6 +127,7 @@ public class NotesList extends ListActivity implements ListView.OnScrollListener
 	NotesListCursorAdapter mAdapter;
 	
 	String mLastFilter;
+        String mSelectedTag;
 	
 	private Handler mHandler = new Handler();
 	
@@ -135,7 +140,10 @@ public class NotesList extends ListActivity implements ListView.OnScrollListener
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		if (debug) Log.d(TAG, "onCreate() " + (savedInstanceState == null ? "(no bundle)" : "(with bundle)"));
+
+        if (debug) {
+            Log.d(TAG, "onCreate() " + (savedInstanceState == null ? "(no bundle)" : "(with bundle)"));
+        }
 
 		if (!EulaActivity.checkEula(this)) {
 			return;
@@ -210,10 +218,63 @@ public class NotesList extends ListActivity implements ListView.OnScrollListener
 			setTitle(R.string.title_pick_note_for_shortcut);
 		}
 		
+        updateTagList();
+
+        Cursor cur = mAdapter.runQueryOnBackgroundThread(null, null);
+
+        cur.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                NotesList.this.updateTagList();
+            }
+        });
+
+        Spinner s = (Spinner) findViewById(R.id.tagselection);
+            s.setOnItemSelectedListener(
+                    new AdapterView.OnItemSelectedListener() {
+
+                        public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                            NotesList.this.updateQuery();
+                        }
+
+                        public void onNothingSelected(AdapterView<?> arg0) {
+                            
+                        }
+                    });
+
 		mDecryptionFailed = false;
 		mDecryptionSucceeded = false;
 	}
 
+    protected void updateTagList() {
+
+        List<String> tags = new ArrayList<String>();
+
+        tags.add(getString(R.string.tag_no_filter));
+
+        Uri notesUri = getIntent().getData();
+
+        Cursor managedCursor = getContentResolver().query(notesUri,
+                new String[]{"tags"},
+                null,
+                null,
+                null);
+
+        if (managedCursor.moveToFirst()) {
+            do {
+                for (String tag : managedCursor.getString(0).split(",")) {
+                    if (!tags.contains(tag.trim())) {
+                        tags.add(tag.trim());
+                    }
+                }
+            } while (managedCursor.moveToNext());
+        }
+
+        Spinner s = (Spinner) findViewById(R.id.tagselection);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, tags);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);
+    }
 	
 	@Override
 	protected void onResume() {
@@ -252,7 +313,7 @@ public class NotesList extends ListActivity implements ListView.OnScrollListener
 			//Cursor cursor = getContentResolver().query(getIntent().getData(), NotesListCursorUtils.PROJECTION, null,
 			//		null, Notes.DEFAULT_SORT_ORDER);
 	
-			Cursor cursor = mCursorUtils.query(null);
+            Cursor cursor = mCursorUtils.query(null, null);
 			
 			/*
 			// Used to map notes entries from the database to views
@@ -263,15 +324,29 @@ public class NotesList extends ListActivity implements ListView.OnScrollListener
 			mAdapter = new NotesListCursorAdapter(this, cursor, mCursorUtils);
 			setListAdapter(mAdapter);
 			
-			if (debug) Log.i(TAG, "Lastfilter: " + mLastFilter);
+            updateQuery();
+        } else {
+            mAdapter.getCursor().requery();
+        }
+    }
 			
-			if (mLastFilter != null) {
-				cursor = mAdapter.runQueryOnBackgroundThread(mLastFilter);
-				mAdapter.changeCursor(cursor);
+    protected void updateQuery() {
+        if (debug) {
+            Log.i(TAG, "Lastfilter: " + mLastFilter);
 			}
+
+        Spinner s = (Spinner) findViewById(R.id.tagselection);
+
+        if ( s.getSelectedItemPosition() == 0 ) {
+            mSelectedTag = null;
 		} else {
-			mAdapter.getCursor().requery();
+            mSelectedTag = (String) s.getSelectedItem();
 		}
+
+        //if (mLastFilter != null || mSelectedTag != null) {
+            Cursor cursor = mAdapter.runQueryOnBackgroundThread(mLastFilter, mSelectedTag);
+            mAdapter.changeCursor(cursor);
+        //}
 	}
 
 	@Override

NotePad/res/layout/noteslist.xml
@@ -33,7 +33,12 @@
             
     </LinearLayout>
      -->
-     
+    <Spinner
+        android:id="@+id/tagselection"
+        android:layout_width="fill_parent"
+        android:layout_height="wrap_content"
+        android:drawSelectorOnTop="false"
+    />
     <!-- The frame layout is here since we will be showing either
     the empty view or the list view.  -->
     <FrameLayout

NotePad/res/values-de/strings.xml
@@ -196,6 +196,6 @@ Aktuelle Version: %s</string>
     <string name="preference_extensions_market_summary">Nach Erweiterungen auf Android Market suchen</string>
     <string name="preference_extensions_developer">OpenIntents</string>
     <string name="preference_extensions_developer_summary">Erweiterungen von der OpenIntents Homepage herunterladen</string>
-	
+    <string name="tag_no_filter">Kein Filter</string>
 	     
 </resources>

NotePad/res/values/strings.xml
@@ -197,6 +197,5 @@ Current version: %s
     <string name="preference_extensions_market_summary">Search for extensions at Android Market.</string>
     <string name="preference_extensions_developer">OpenIntents</string>
     <string name="preference_extensions_developer_summary">Get extensions from the OpenIntents homepage.</string>
-	
-	     
+    <string name="tag_no_filter">No filter</string>
 </resources>

NotePad/src/org/openintents/notepad/noteslist/NotesListCursor.java
@@ -17,6 +17,7 @@ import android.database.Cursor;
 import android.os.Handler;
 import android.text.TextUtils;
 import android.util.Log;
+import java.util.ArrayList;
 
 public class NotesListCursor extends OpenMatrixCursor {
 
@@ -72,6 +73,7 @@ public class NotesListCursor extends OpenMatrixCursor {
 	Cursor mDbCursor;
 	
 	public String mCurrentFilter;
+        public String mSelectedTag;
 	
 	/**
 	 * Map encrypted titles to decrypted ones.
@@ -119,7 +121,7 @@ public class NotesListCursor extends OpenMatrixCursor {
 
 	@Override
 	public boolean requery() {
-		runQuery(mCurrentFilter);
+		runQuery(mCurrentFilter, mSelectedTag);
 		
 		return super.requery();
 	}
@@ -130,9 +132,9 @@ public class NotesListCursor extends OpenMatrixCursor {
 	 * 
 	 * @param constraint
 	 */
-	public Cursor query(CharSequence constraint) {
+	public Cursor query(CharSequence constraint, String tag) {
 		NotesListCursor cursor = new NotesListCursor(mContext, mIntent);
-		cursor.runQuery(constraint);
+		cursor.runQuery(constraint, tag);
 		return cursor;
 	}
 	
@@ -141,7 +143,7 @@ public class NotesListCursor extends OpenMatrixCursor {
 	 * 
 	 * @param constraint
 	 */
-	private void runQuery(CharSequence constraint) {
+	private void runQuery(CharSequence constraint, String tag) {
 
 		// We have to query all items and return a new object, because notes may be encrypted.
 
@@ -150,6 +152,11 @@ public class NotesListCursor extends OpenMatrixCursor {
 		} else {
 			mCurrentFilter = null;
 		}
+                if(tag != null) {
+                    mSelectedTag = tag;
+                } else {
+                    mSelectedTag = null;
+                }
 		
 		if (mDbCursor != null) {
 			mDbCursor.unregisterContentObserver(mContentObserver);
@@ -224,7 +231,7 @@ public class NotesListCursor extends OpenMatrixCursor {
 			
 			boolean addrow = false;
 			
-			if (TextUtils.isEmpty(mCurrentFilter)) {
+			if (TextUtils.isEmpty(mCurrentFilter) && TextUtils.isEmpty(mSelectedTag)) {
 				// Add all rows if there is no filter.
 				addrow = true;
 			} else if (skipEncrypted) {
@@ -233,8 +240,6 @@ public class NotesListCursor extends OpenMatrixCursor {
 				// test the filter
 
 				// Build search string from title and tags.
-				String searchstring = null;
-				if (!TextUtils.isEmpty(mCurrentFilter)) {
 					StringBuilder sb = new StringBuilder();
 					sb.append(" ");
 					sb.append(title.toUpperCase());
@@ -243,11 +248,25 @@ public class NotesListCursor extends OpenMatrixCursor {
 						String spacetags = tags.replace(",", " ");
 						sb.append(spacetags.toUpperCase());
 					}
-					searchstring = sb.toString();
+                            String searchstring = sb.toString();
+
+                            List<String> tagList = new ArrayList<String>();
+                            if (!TextUtils.isEmpty(tags)) {
+                                for(String tagString: tags.split(",")) {
+                                    if(tagString.trim().length() != 0) {
+                                        tagList.add(tagString.trim());
+                                    }
+                                }
 				}
 				
-				// apply filter:
+                            if ( TextUtils.isEmpty(mCurrentFilter) ) {
+                                addrow = tagList.contains(mSelectedTag.trim());
+                            } else if ( TextUtils.isEmpty(mSelectedTag) ) {
 				addrow = searchstring.contains(" " + mCurrentFilter.toUpperCase());
+                            } else {
+				addrow = searchstring.contains(" " + mCurrentFilter.toUpperCase()) &&
+                                         tagList.contains(mSelectedTag.trim());
+                            }
 			}
 			
 			if (addrow) {

NotePad/src/org/openintents/notepad/noteslist/NotesListCursorAdapter.java
@@ -109,8 +109,7 @@ public class NotesListCursorAdapter extends CursorAdapter {
 	}
 */
 	
-	@Override
-	public Cursor runQueryOnBackgroundThread(CharSequence constraint) {
+	public Cursor runQueryOnBackgroundThread(CharSequence constraint, String tag) {
 		//Log.i(TAG, "runQueryOnBackgroundThread " + constraint + ", " + mIntent.getData());
 
 		
@@ -121,7 +120,7 @@ public class NotesListCursorAdapter extends CursorAdapter {
 				new String[] { }, Notes.DEFAULT_SORT_ORDER);
 		*/
 		
-		Cursor cursor = mCursorUtils.query(constraint);
+		Cursor cursor = mCursorUtils.query(constraint, tag);
 		
 		return cursor;
 	}

commit 75f4722f61ca8a0f262d3e4294f00b9554a9ed84

OI Notepad: Fixed null-pointer exception if notepad contains notes with empty tags (introduced in r2430). (also renamed tags -> taglist)
git-svn-id: https://openintents.googlecode.com/svn/trunk@2431 72b678ce-9140-0410-bee8-679b907dd61a

NotePad/src/org/openintents/notepad/noteslist/NotesList.java
@@ -248,9 +248,9 @@ public class NotesList extends ListActivity implements ListView.OnScrollListener
 
     protected void updateTagList() {
 
-        List<String> tags = new ArrayList<String>();
+        List<String> taglist = new ArrayList<String>();
 
-        tags.add(getString(R.string.tag_no_filter));
+        taglist.add(getString(R.string.tag_no_filter));
 
         Uri notesUri = getIntent().getData();
 
@@ -262,16 +262,19 @@ public class NotesList extends ListActivity implements ListView.OnScrollListener
 
         if (managedCursor.moveToFirst()) {
             do {
-                for (String tag : managedCursor.getString(0).split(",")) {
-                    if (!tags.contains(tag.trim())) {
-                        tags.add(tag.trim());
-                    }
-                }
+            	String tags = managedCursor.getString(0);
+            	if (!TextUtils.isEmpty(tags)) {
+	                for (String tag : tags.split(",")) {
+	                    if (!taglist.contains(tag.trim())) {
+	                        taglist.add(tag.trim());
+	                    }
+	                }
+            	}
             } while (managedCursor.moveToNext());
         }
 
         Spinner s = (Spinner) findViewById(R.id.tagselection);
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, tags);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, taglist);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s.setAdapter(adapter);
     }