d762d4bce48c524bdbebc37921327ec9fee1dbce
Peli <peli0101@gmail.com>
2011-01-23 20:49:31 +0000

    branches/distributionlibrary: Create new task to update res/raw/recent_changes.txt automatically from readme.txt.
    git-svn-id: https://openintents.googlecode.com/svn/branches/distributionlibrary@3057 72b678ce-9140-0410-bee8-679b907dd61a

--- distribution/DistributionLibrary/src/org/openintents/distribution/UpdateDialog.java
+++ distribution/DistributionLibrary/src/org/openintents/distribution/UpdateDialog.java
@@ -72,7 +72,12 @@ public class UpdateDialog extends DownloadAppDialog {
 		final Intent intent  = new Intent(Intent.ACTION_VIEW);
 		
     	if (which == BUTTON1) {
-			intent.setData(Uri.parse(mContext.getString(R.string.oi_distribution_update_app_developer_url)));
+    		
+    		// TODO: Obtain this resId properly from
+    		// Manifest or about.xml
+    		int resId = R.string.about_website_url;
+    		
+			intent.setData(Uri.parse(mContext.getString(resId)));
 			startSaveActivity(intent);
     	} else {
     		// BUTTON2 is handled by parent.
