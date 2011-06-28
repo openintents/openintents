b2a48c1c14592dac3bfc7cea4a2ea330e1ae5866
friedger <friedger@gmail.com>
2011-06-28 11:31:15 +0000

    removed MainActivity as parameter
    git-svn-id: https://openintents.googlecode.com/svn/trunk@3441 72b678ce-9140-0410-bee8-679b907dd61a

--- historify/HistorifyTest/src/org/openintents/historify/test/MainActivityTest.java
+++ historify/HistorifyTest/src/org/openintents/historify/test/MainActivityTest.java
@@ -2,16 +2,17 @@ package org.openintents.historify.test;
 
 import org.openintents.historify.MainActivity;
 
+import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
 
-public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+public class MainActivityTest extends ActivityInstrumentationTestCase2 {
 
     public MainActivityTest() {
         super(MainActivity.class);
     }
 
     public void testActivity() {
-        MainActivity activity = getActivity();
+        Activity activity = getActivity();
         assertNotNull(activity);
     }
 }
