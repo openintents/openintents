65dce36a7a0beb2d806e9c22016939448a588f79
rmceoin <rmceoin@gmail.com>
2011-11-29 02:06:25 +0000

    OI Countdown Test: GCI task by Reid Anderson: added new project http://www.google-melange.com/gci/task/view/google/gci2011/7115245
    git-svn-id: https://openintents.googlecode.com/svn/trunk@3770 72b678ce-9140-0410-bee8-679b907dd61a

---
+++ countdown/CountdownTest/src/org/openintents/countdown/test/TestCountdownActivity.java
@@ -0,0 +1,241 @@
+package org.openintents.countdown.test;
+
+import java.util.Random;
+
+import org.openintents.com.android.internal.widget.NumberPickerButton;
+import org.openintents.countdown.R;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.test.InstrumentationTestCase;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import com.jayway.android.robotium.solo.Solo;
+
+public class TestCountdownActivity extends InstrumentationTestCase {
+	
+	private static final String TAG = "TestCountdownActivity";
+	
+	private Solo solo;
+	private Activity activity;
+	private Random random = new Random();
+	
+	public TestCountdownActivity() {
+		super();
+	}
+	
+	protected void setUp() throws Exception {
+		Intent i = new Intent();
+		i.setAction("android.intent.action.MAIN");
+		i.setClassName("org.openintents.countdown",
+				"org.openintents.countdown.list.CountdownListActivity");
+		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+		activity = getInstrumentation().startActivitySync(i);
+
+		this.solo = new Solo(getInstrumentation(), activity);
+	}
+	
+	@Override
+	public void tearDown() throws Exception {
+		try {
+			solo.finalize(); 	//Robotium will finish all the activities that have been open
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		super.tearDown();
+	}
+	
+	/*
+	 * Test adding a countdown
+	 */
+	public void testAddCountdownToList() {
+		String itemname = "testitem_add_" + random.nextInt(10000);
+		
+		// Add the countdown and name it
+		solo.clickOnMenuItem("Add countdown");
+		solo.enterText(0, itemname);
+		
+		solo.goBack();
+		
+		// Verify that the countdown was added
+		assertTrue(solo.searchText(itemname));
+	}
+	
+	/*
+	 * Test deleting a countdown using a longpress
+	 */
+	public void testDeleteCountdownFromList() {
+		String itemname = "testitem_delete_" + random.nextInt(10000);
+		
+		// Add the item
+		solo.clickOnMenuItem("Add countdown");
+		solo.enterText(0,itemname);
+		solo.goBack();
+		assertTrue(solo.searchText(itemname));
+		
+		// Long press to open the context menu
+		solo.clickLongOnText(itemname);
+		
+		// Make sure context menu opened:
+		assertTrue(solo.searchText("Edit"));
+		assertTrue(solo.searchText("Delete"));
+		
+		// Delete the item and verify it got deleted
+		solo.clickOnText("Delete");
+		assertFalse(solo.searchText(itemname));
+	}
+	
+	/*
+	 * Test adding and then running a countdown
+	 */
+	public void testAddAndRunCountdown() {
+		String itemname = "testitem_add_and_run_" + random.nextInt(10000);
+		
+		solo.clickOnMenuItem("Add countdown");
+		solo.enterText(0,itemname);
+		
+		// Increment everything in the duration picker by 1
+		for(View v: solo.getViews()) {
+			if(v instanceof NumberPickerButton) {
+				if(v.getId() == R.id.increment) {
+					solo.clickOnView(v);
+					solo.sleep(3000);
+				}
+			}
+		}
+		
+		// Start the countdown
+		solo.clickOnButton("Start");
+		
+		// Verify that the time is correct and that they stop and modify buttons are present
+		assertTrue(solo.searchText("1:0"));
+		assertTrue(solo.searchButton("Stop"));
+		assertTrue(solo.searchButton("Modify"));
+		
+		// Stop the countdown and verify the start button reappears
+		solo.clickOnButton("Stop");
+		assertTrue(solo.searchButton("Start"));
+	}
+	
+	/*
+	 * Test that the notification settings behave as they should
+	 */
+	public void testNotificationSettings() {
+		String itemname = "testitem_notification_settings_" + random.nextInt(10000);
+		
+		solo.clickOnMenuItem("Add countdown");
+		solo.enterText(0,itemname);
+		
+		// Click on the Ring checkbox and verify that the setting changes, and all others don't
+		solo.clickOnCheckBox(1);
+		assertFalse(solo.isCheckBoxChecked("Ring"));
+		assertTrue(solo.isCheckBoxChecked("Vibrate"));
+		assertTrue(solo.isCheckBoxChecked("Light"));
+		assertTrue(solo.searchText("Light"));
+		
+		// Click on Notification checkbox and verify it's unchecked
+		// Then verify all other settings disappear
+		solo.clickOnCheckBox(0);
+		assertFalse(solo.isCheckBoxChecked("Notification in status bar"));
+		assertFalse(solo.searchText("Ring",true));
+		assertFalse(solo.searchText("Vibrate",true));
+		assertFalse(solo.searchText("Light",true));
+	}
+	
+	/*
+	 * Test the button which adds a day picker
+	 */
+	public void testDayCountdown() {
+		String itemname = "testitem_day_picker_" + random.nextInt(10000);
+		
+		solo.clickOnMenuItem("Add countdown");
+		solo.enterText(0,itemname);
+		
+		// Click on the + to add the day picker
+		// Verify there are 4 increment buttons rather than 3
+		solo.clickOnButton(0);
+		int numIncrementButtons = 0;
+		for(View v: solo.getViews()) {
+			if(v instanceof NumberPickerButton) {
+				if(v.getId() == R.id.increment) {
+					numIncrementButtons++;
+				}
+			}
+		}
+		assertTrue(numIncrementButtons == 4);
+	}
+	
+	/*
+	 * Test setting the countdown using set date rather than set duration
+	 */
+	public void testSetDate() {
+		String itemname = "testitem_date_picker_" + random.nextInt(10000);
+		
+		solo.clickOnMenuItem("Add countdown");
+		solo.enterText(0,itemname);
+		
+		// Change the mode from Set duration to Set date
+		solo.clickOnMenuItem("Set date");
+		
+		// Set the time of day
+		solo.clickOnButton(1);
+		TimePicker time;
+		time = solo.getCurrentTimePickers().get(0);
+		int hour = 10;
+		int minute = 16;
+		solo.setTimePicker(time, hour, minute);
+		solo.clickOnButton("Set");
+		
+		// Verify the time of day that we set is displayed
+		assertTrue(solo.searchButton(hour+":"+minute));
+		
+		// Set the date (Robotium numbers months starting from 0 so Dec. is 11)
+		solo.clickOnButton(0);
+		DatePicker date;
+		date = solo.getCurrentDatePickers().get(0);
+		int day = 21;
+		int month = 11;
+		int year = 2012;
+		solo.setDatePicker(date, year, month, day);
+		solo.clickOnButton("Set");
+		
+		// Verify the date that we set is displayed
+		assertTrue(solo.searchButton((month+1)+"/"+day+"/"+year));
+	}
+	
+	/*
+	 * Test deleting a countdown from the menu rather than with a longpress
+	 */
+	public void testDeleteFromMenu() {
+		String itemname = "testitem_delete_from_menu_" + random.nextInt(10000);
+		
+		solo.clickOnMenuItem("Add countdown");
+		solo.enterText(0,itemname);
+		
+		solo.clickOnMenuItem("Delete");
+		
+		// Verify the item got deleted
+		assertFalse(solo.searchText(itemname));
+	}
+	
+	/*
+	 * Test that the notification setting is stored like it should be.
+	 */
+	public void testNotificationSetting() {
+		// Go to the preferences and set the Notification timeout
+		solo.clickOnMenuItem("Settings");
+		solo.clickOnText("Notification timeout");
+		solo.clickOnText("1 minute");
+		
+		// Return to the main activity
+		solo.goBack();
+		
+		// Go to preferences and verify the correct preference is checked
+		solo.clickOnMenuItem("Settings");
+		solo.clickOnText("Notification timeout");
+		assertTrue(solo.isTextChecked("1 minute"));
+	}
+}
