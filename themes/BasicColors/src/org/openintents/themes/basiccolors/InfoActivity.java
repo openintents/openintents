09e0283a00ff4f9790d74d321f6bbd7eb1749bb2
friedger <friedger@gmail.com>
2012-06-05 12:35:32 +0200

    format

--- themes/BasicColors/src/org/openintents/themes/basiccolors/InfoActivity.java
+++ themes/BasicColors/src/org/openintents/themes/basiccolors/InfoActivity.java
@@ -26,99 +26,99 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class InfoActivity extends ListActivity implements OnItemClickListener {
 
-    private int[] mApplications = {
-            R.string.info_app_notepad, // OI Notepad
-            R.string.info_app_shopping // OI Shopping List
-    };
-    
-    private String[] mPackageNames = {
-    		"org.openintents.notepad", // OI Notepad
-    		"org.openintents.shopping" // OI Shopping List
-    };
-    
-    private int[] mMinVersionCodes = {
-    		10059, // OI Notepad
-    		10013 // OI Shopping List
-    };
-    
-    private String[] mMinVersionName = {
-    		"1.2.0", // OI Notepad
-    		"1.2.1" // OI Shopping List
-    };
-    
-    private int[] mInfoText = {
-    		R.string.info_text_notepad,
-    		R.string.info_text_shopping
-    };
-    
-    private String[] mMarketUris = {
-    		"market://search?q=pname:org.openintents.notepad", // OI Notepad
-    		"market://search?q=pname:org.openintents.shopping" // OI Shopping List
-    };
-    
-    private String[] mDeveloperUris = {
-    		"http://www.openintents.org/en/notepad", // OI Notepad
-    		"http://www.openintents.org/en/shoppinglist" // OI Shopping List
-    };
+	private int[] mApplications = { R.string.info_app_notepad, // OI Notepad
+			R.string.info_app_shopping // OI Shopping List
+	};
 
-    private String[] mIntentAction = {
-    		Intent.ACTION_VIEW, // OI Notepad
-    		Intent.ACTION_VIEW // OI Shopping List
-    };
-    
-    private String[] mIntentData = {
-    		"content://org.openintents.notepad/notes", // OI Notepad
-    		"content://org.openintents.shopping/items" // OI Shopping List
-    };
-    
-    // Theme for the Info activity
-    private int mTheme = R.style.Theme_BasicColors_Blue_Notepad;
-    
-    /////////////////////////////////////////////////////////////////////////////
-    
+	private String[] mPackageNames = { "org.openintents.notepad", // OI Notepad
+			"org.openintents.shopping" // OI Shopping List
+	};
+
+	private int[] mMinVersionCodes = { 10059, // OI Notepad
+			10013 // OI Shopping List
+	};
+
+	private String[] mMinVersionName = { "1.2.0", // OI Notepad
+			"1.2.1" // OI Shopping List
+	};
+
+	private int[] mInfoText = { R.string.info_text_notepad,
+			R.string.info_text_shopping };
+
+	private String[] mMarketUris = {
+			"market://search?q=pname:org.openintents.notepad", // OI Notepad
+			"market://search?q=pname:org.openintents.shopping" // OI Shopping
+																// List
+	};
+
+	private String[] mDeveloperUris = {
+			"http://www.openintents.org/en/notepad", // OI Notepad
+			"http://www.openintents.org/en/shoppinglist" // OI Shopping List
+	};
+
+	private String[] mIntentAction = { Intent.ACTION_VIEW, // OI Notepad
+			Intent.ACTION_VIEW // OI Shopping List
+	};
+
+	private String[] mIntentData = { "content://org.openintents.notepad/notes", // OI
+																				// Notepad
+			"content://org.openintents.shopping/items" // OI Shopping List
+	};
+
+	// Theme for the Info activity
+	private int mTheme = R.style.Theme_BasicColors_Blue_Notepad;
+
+	// ///////////////////////////////////////////////////////////////////////////
+
 	public static final int DIALOG_INFO = 0;
 	public static final int DIALOG_GET_FROM_MARKET = 100;
-	
-    private String[] mApplicationStrings;
-    private Typeface mTypeface;
-    private int mTextSizeMedium;
-    private int mTextSizeLarge;
-    private int mTextColor;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.infoactivity);
+	private String[] mApplicationStrings;
+	private Typeface mTypeface;
+	private int mTextSizeMedium;
+	private int mTextSizeLarge;
+	private int mTextColor;
 
-        mApplicationStrings = new String[mApplications.length];
-        for (int i = 0; i < mApplications.length; i++) {
-        	mApplicationStrings[i] = getString(mApplications[i]);
-        }
-        setListAdapter(new FontArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, mApplicationStrings));
-        ListView listview = getListView();
-        
-        listview.setOnItemClickListener(this);
-        
-        TypedArray a = obtainStyledAttributes(mTheme, R.styleable.ShoppingList);
-		String typefaceName = a.getString(R.styleable.ShoppingList_textTypeface);
-	    mTextSizeMedium = a.getDimensionPixelOffset(R.styleable.ShoppingList_textSizeMedium, 23);
-	    mTextSizeLarge = a.getDimensionPixelOffset(R.styleable.ShoppingList_textSizeLarge, 28);
-	    mTextColor = a.getColor(R.styleable.ShoppingList_textColor, Color.BLACK);
-	    Drawable background = a.getDrawable(R.styleable.ShoppingList_background);
-	    
-	    View v = findViewById(R.id.background);
-	    v.setBackgroundDrawable(background);
-	    
-		mTypeface = Typeface.createFromAsset(getResources().getAssets(), typefaceName);
-        
-        TextView tv = (TextView) findViewById(R.id.text);
-        tv.setTypeface(mTypeface);
-        tv.setTextSize(mTextSizeMedium);
-        tv.setTextColor(mTextColor);
-        
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.infoactivity);
+
+		mApplicationStrings = new String[mApplications.length];
+		for (int i = 0; i < mApplications.length; i++) {
+			mApplicationStrings[i] = getString(mApplications[i]);
+		}
+		setListAdapter(new FontArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, mApplicationStrings));
+		ListView listview = getListView();
+
+		listview.setOnItemClickListener(this);
+
+		TypedArray a = obtainStyledAttributes(mTheme, R.styleable.ShoppingList);
+		String typefaceName = a
+				.getString(R.styleable.ShoppingList_textTypeface);
+		mTextSizeMedium = a.getDimensionPixelOffset(
+				R.styleable.ShoppingList_textSizeMedium, 23);
+		mTextSizeLarge = a.getDimensionPixelOffset(
+				R.styleable.ShoppingList_textSizeLarge, 28);
+		mTextColor = a
+				.getColor(R.styleable.ShoppingList_textColor, Color.BLACK);
+		Drawable background = a
+				.getDrawable(R.styleable.ShoppingList_background);
+
+		View v = findViewById(R.id.background);
+		v.setBackgroundDrawable(background);
+
+		mTypeface = Typeface.createFromAsset(getResources().getAssets(),
+				typefaceName);
+
+		TextView tv = (TextView) findViewById(R.id.text);
+		tv.setTypeface(mTypeface);
+		tv.setTextSize(mTextSizeMedium);
+		tv.setTextColor(mTextColor);
+
+	}
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
@@ -128,7 +128,7 @@ public class InfoActivity extends ListActivity implements OnItemClickListener {
 			showDialog(DIALOG_GET_FROM_MARKET + pos);
 		}
 	}
-	
+
 	private class FontArrayAdapter<T> extends ArrayAdapter<T> {
 
 		public FontArrayAdapter(Context context, int textViewResourceId,
@@ -139,23 +139,24 @@ public class InfoActivity extends ListActivity implements OnItemClickListener {
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			// TODO Auto-generated method stub
-			TextView tv = (TextView) super.getView(position, convertView, parent);
-			
+			TextView tv = (TextView) super.getView(position, convertView,
+					parent);
+
 			tv.setTypeface(mTypeface);
 			tv.setTextSize(mTextSizeLarge);
-	        tv.setTextColor(mTextColor);
-			
+			tv.setTextColor(mTextColor);
+
 			return tv;
 		}
-		
+
 	}
 
-
 	/**
-	 * Indicates whether a specific package with minimum version code is available.
+	 * Indicates whether a specific package with minimum version code is
+	 * available.
 	 */
-	public static boolean isPackageAvailable(final Context context, final String packageName,
-			final int minVersionCode) {
+	public static boolean isPackageAvailable(final Context context,
+			final String packageName, final int minVersionCode) {
 		boolean result = false;
 		try {
 			PackageInfo pi = context.getPackageManager().getPackageInfo(
@@ -163,16 +164,16 @@ public class InfoActivity extends ListActivity implements OnItemClickListener {
 			if (pi.versionCode >= minVersionCode) {
 				result = true;
 			}
-        } catch (PackageManager.NameNotFoundException e) {
-        	
-        }
-	    return result;
+		} catch (PackageManager.NameNotFoundException e) {
+
+		}
+		return result;
 	}
 
 	@Override
 	protected Dialog onCreateDialog(int id) {
 		Dialog dialog = null;
-		
+
 		if (id < DIALOG_GET_FROM_MARKET) {
 			dialog = buildInfoDialog(id - DIALOG_INFO);
 		} else {
@@ -186,20 +187,20 @@ public class InfoActivity extends ListActivity implements OnItemClickListener {
 
 	private AlertDialog buildInfoDialog(final int pos) {
 		String infotext = getString(mInfoText[pos]);
-		String infolaunch = getString(R.string.info_launch, mApplicationStrings[pos]);
+		String infolaunch = getString(R.string.info_launch,
+				mApplicationStrings[pos]);
 		return new AlertDialog.Builder(this)
-			//.setIcon(RD.drawable.icon)
-			.setTitle(R.string.app_name)
-			.setMessage(infotext)
-			.setPositiveButton(infolaunch,
-					new DialogInterface.OnClickListener() {
-						public void onClick(DialogInterface dialog,
-								int whichButton) {
-							// click Ok
-							launchApplication(pos);
-						}
-					})
-			.create();
+				// .setIcon(RD.drawable.icon)
+				.setTitle(R.string.app_name)
+				.setMessage(infotext)
+				.setPositiveButton(infolaunch,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								// click Ok
+								launchApplication(pos);
+							}
+						}).create();
 	}
 
 	void launchApplication(int pos) {
@@ -210,73 +211,75 @@ public class InfoActivity extends ListActivity implements OnItemClickListener {
 		try {
 			startActivity(intent);
 		} catch (ActivityNotFoundException e) {
-			
+
 		}
 	}
 
 	private AlertDialog buildGetFromMarketDialog(int pos) {
-		String info_not_available = getString(R.string.info_not_available, mApplicationStrings[pos], mMinVersionName[pos]);
+		String info_not_available = getString(R.string.info_not_available,
+				mApplicationStrings[pos], mMinVersionName[pos]);
 		String info_get = getString(R.string.info_get, mApplicationStrings[pos]);
-		return new GetFromMarketDialog(this, 
-				info_not_available,
-				info_get,
-				mMarketUris[pos],
-				mDeveloperUris[pos]);
-	
+		return new GetFromMarketDialog(this, info_not_available, info_get,
+				mMarketUris[pos], mDeveloperUris[pos]);
+
 	}
-	
-	private class GetFromMarketDialog extends AlertDialog implements OnClickListener {
+
+	private class GetFromMarketDialog extends AlertDialog implements
+			OnClickListener {
 		private static final String TAG = "GetFromMarketDialog";
 
-	    Context mContext;
-	    String mMarketUri;
-	    String mDeveloperUri;
-	    
-	    public GetFromMarketDialog(Context context, String message, String buttontext, String market_uri, String developer_uri) {
-	        super(context);
-	        mContext = context;
-	        mMarketUri = market_uri;
-	        mDeveloperUri = developer_uri;
+		Context mContext;
+		String mMarketUri;
+		String mDeveloperUri;
 
-	        //setTitle(context.getText(R.string.menu_edit_tags));
-	        setMessage(message);
-	    	setButton(buttontext, this);
-	        
-	    }
+		public GetFromMarketDialog(Context context, String message,
+				String buttontext, String market_uri, String developer_uri) {
+			super(context);
+			mContext = context;
+			mMarketUri = market_uri;
+			mDeveloperUri = developer_uri;
+
+			// setTitle(context.getText(R.string.menu_edit_tags));
+			setMessage(message);
+			setButton(buttontext, this);
+
+		}
 
 		public void onClick(DialogInterface dialog, int which) {
-	    	if (which == BUTTON1) {
-	    		Uri uri = Uri.parse(mMarketUri);
-	    		
+			if (which == BUTTON1) {
+				Uri uri = Uri.parse(mMarketUri);
+
 				Intent intent = new Intent(Intent.ACTION_VIEW);
 				intent.setData(uri);
-				
-				uri= Uri.parse(mDeveloperUri);
+
+				uri = Uri.parse(mDeveloperUri);
 				Intent intent2 = new Intent(Intent.ACTION_VIEW);
 				intent2.setData(uri);
 				startSaveActivity(mContext, intent, intent2);
-	    	}
-			
+			}
+
 		}
 	}
-	
+
 	/**
-	 * Start an activity but prompt a toast if activity is not found
-	 * (instead of crashing).
+	 * Start an activity but prompt a toast if activity is not found (instead of
+	 * crashing).
 	 * 
 	 * @param context
 	 * @param intent
-	 * @param intent2 Alternative intent to call, if the first is not reachable
+	 * @param intent2
+	 *            Alternative intent to call, if the first is not reachable
 	 */
-	public static void startSaveActivity(Context context, Intent intent, Intent intent2) {
+	public static void startSaveActivity(Context context, Intent intent,
+			Intent intent2) {
 		try {
 			context.startActivity(intent);
 		} catch (ActivityNotFoundException e) {
-			//Log.e(TAG, "Error starting activity.", e);
+			// Log.e(TAG, "Error starting activity.", e);
 			try {
 				context.startActivity(intent2);
 			} catch (ActivityNotFoundException e2) {
-				//Log.e(TAG, "Error starting second activity.", e2);
+				// Log.e(TAG, "Error starting second activity.", e2);
 			}
 		}
 	}
